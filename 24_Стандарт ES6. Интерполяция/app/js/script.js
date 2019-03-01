1771be809f7683aba10fc2d0caac1f5c6f8a034c
Хантимиров Фанис Минегалиевич <Khantimirov_FM@kvz.ru>
2019-03-01 17:11:50 +0300

    01/03/19

---
+++ 24_Стандарт ES6. Интерполяция/app/js/script.js
@@ -0,0 +1,5 @@
+let name = 'Ivan',
+    age = 30,
+    mail = 'ex@mail.ru';
+
+document.write(`Пользователю ${name} ${age} лет. Его почтовый адрес: ${mail}`);
---
+++ 25_ES6. let и const/app/js/script.js
@@ -0,0 +1,18 @@
+function makeArray() {
+    var items = [];
+
+    for (let i = 0; i < 10; i++) {
+        var item = function () {
+            console.log(i);
+        };
+        items.push(item);
+
+        return items;
+    }
+}
+
+var arr = makeArray();
+
+arr[1]();
+arr[3]();
+arr[7]();
