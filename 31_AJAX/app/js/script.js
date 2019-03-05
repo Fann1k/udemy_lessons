d74e351fab22e66d1682f569dc9e6f4009ae14db
Хантимиров Фанис Минегалиевич <Khantimirov_FM@kvz.ru>
2019-03-05 17:11:38 +0300

    05/03/19

---
+++ 31_AJAX/app/js/script.js
@@ -0,0 +1,8 @@
+let inputRub = document.getElementById('rub'),
+    inputUsd = document.getElementById('usd');
+
+inputRub.addEventListener('input', () => {
+    let request = new XMLHttpRequest();
+
+
+})
