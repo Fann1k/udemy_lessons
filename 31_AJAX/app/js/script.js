4b94151ff5011a3187eb5d0f60990314d8772ce3
Хантимиров Фанис Минегалиевич <Khantimirov_FM@kvz.ru>
2019-03-06 16:07:02 +0300

    06/03/19

--- 31_AJAX/app/js/script.js
+++ 31_AJAX/app/js/script.js
@@ -2,7 +2,23 @@ let inputRub = document.getElementById('rub'),
     inputUsd = document.getElementById('usd');
 
 inputRub.addEventListener('input', () => {
-    let request = new XMLHttpRequest();
+    let request = new XMLHttpRequest(); //создаем объект XMLHttpRequest - главный объект для работы с AJAX запросами со своими методами, св-ами, событиями
 
+    request.open('GET', 'js/current.json'); //метод open производит настройку AJAX запроса
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8'); //настройка http запросов (указываем контент - json в кодировке utf-8)
+    request.send(); //отправка запроса на сервер (тело запроса, даннные отправляемые на сервер)
 
-})
+    //типы ответов от сервера:
+    //status - 404, 0, 200, 403 и т.д
+    //statusText - текстовое описание ответа - ok, not found
+    //responseText / response - текст ответа сервера, то что бэкэндщик отправляет 
+    //readyState - текущее состояние запроса
+    request.addEventListener('readystatechange', function () {
+        if (request.readyState === 4 && request.status == 200) {
+            let data = JSON.parse(request.response);
+            inputUsd.value = inputRub.value / data.usd;
+        } else {
+            inputUsd.value = "Что-то не так!";
+        }
+    });
+});
